f93fe6b36ff3f4111ab70ae44d0e2dd477af20f8
Sergey Shabanov <shsv382@gmail.com>
2024-04-08 20:43:05 +0300

    Убран лишний console.log

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 
 function getMessages(req, res) {
     const token = req.headers.authorization.split(' ')[1]
-    console.log(token)
     jwt.verify(token, process.env.SECRET_JWT_KEY, function(error, decoded) {
         if (error) {
             return res.status(403).json({error})
